0051ab09d8e09e9a1ba304abb0b05542dc8b392f
Gino <ginolencina@gmail.com>
2025-04-16 15:49:46 -0300

    clinica odontologica spring

---
+++ Introduccion al Backend en JAVA/Spring/ClinicaOdontologicaSpring/src/main/java/com/dh/ClinicaOdontologicaSpring/ClinicaOdontologicaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dh.ClinicaOdontologicaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaOdontologicaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaOdontologicaSpringApplication.class, args);
+	}
+
+}
